0b5681bc2ff43a4762d7b9afc1f77916bd1b0af8
Larissa Itimura <litimura@ciandt.com>
2023-03-12 13:29:35 -0300

    finishing the migration from javascript to typescript

--- src/componentes/Time/index.tsx
+++ src/componentes/Time/index.tsx
@@ -23,10 +23,11 @@ const Time = (props: TimeProps) => {
                         nome={colaborador.nome} 
                         cargo={colaborador.cargo} 
                         imagem={colaborador.imagem}
+                        data={colaborador.data}
                     /> )}
             </div>
         </section> 
-        : ''
+        : <></>
     )
 }
 
